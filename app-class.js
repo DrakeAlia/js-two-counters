a0fd73169e5292a989b5197b379c11ecffe83d5c
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-05-25 13:19:00 -0700

    added functionality to index.html and app.class.js files, using class function for the js portion of this project

---
+++ app-class.js
@@ -0,0 +1,50 @@
+function getElement(selection) {
+    const element = document.querySelector(selection)
+    if (element) {
+        return element
+    }
+    throw new Error(
+        `Please check "${selection}" selector, no such 
+        element exists here`
+    )
+}
+
+class Counter {
+    constructor(element, value) {
+        this.counter = element
+        this.value = value
+        this.resetBtn = element.querySelector('.reset')
+        this.increaseBtn = element.querySelector('.increase')
+        this.decreaseBtn = element.querySelector('.decrease')
+        this.valueDOM = element.querySelector('.value')
+        // texContent represents the text content of the node and its descendants.
+        this.valueDOM.textContent = this.value
+        // The bind() method creates a new function that, when called, has its this keyword set to the 
+        // provided value, with a given sequence of arguments preceding any provided when 
+        // the new function is called.
+        this.increase = this.increase.bind(this)
+        this.decrease = this.decrease.bind(this)
+        this.reset = this.reset.bind(this)
+        // sets up a function that will be called whenever the specified event is 
+        // delivered to the target.
+        this.increaseBtn.addEventListener('click', this.increase)
+        this.decreaseBtn.addEventListener('click', this.decrease)
+        this.resetBtn.addEventListener('click', this.reset)
+    }
+    // when the buttons are clicked on
+    increase() {
+        this.value++
+        this.valueDOM.textContent = this.value
+    }
+    decrease() {
+        this.value--
+        this.valueDOM.textContent = this.value
+    }
+    reset() {
+        this.value = 0
+        this.valueDOM.textContent = this.value
+    }
+}
+
+const firstCounter = new Counter(getElement('.first-counter'), 100)
+const secondCounter = new Counter(getElement('.second-counter'), 200)
